49b5494ca625ec1ad73f0928c768ba576d43d6be
ivan-karaberov <karaberovivan123456@gmail.com>
2023-10-10 19:59:09 +0300

    feat: add UserFilter

--- finance-bot/__main__.py
+++ finance-bot/__main__.py
@@ -3,18 +3,25 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 
+from services.filters import UserFilter
 import config
 import handlers
+
 
 COMMAND_HANDLERS = {
     "start": handlers.start,
     "help": handlers.help,
+    "add_expense": handlers.add_expense,
 }
 
 
 def register_message_handlers(dp: Dispatcher) -> None:
     for key, value in COMMAND_HANDLERS.items():
-        dp.message.register(value, Command(key))
+        dp.message.register(
+            value,
+            UserFilter(access_id=config.TELEGRAM_ACCESS_ID),
+            Command(key)
+        )
 
 
 async def main() -> None:
